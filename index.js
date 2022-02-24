9bbe8a3b63abef9d0dd169502e406d5e8e308d94
theirrationalone <anilkushwaha289@gmail.com>
2022-02-24 21:03:37 +0530

    updated gracefully

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log('Quote Generator Application has been started !');
+const nextQuoteBtn = document.getElementById('next-button');
+const quoteDes = document.getElementById('quote-container__quote');
+const quoteAuthor = document.getElementById('author');
+
+const randomQuoteHandler = event => {
+    const xhr = new XMLHttpRequest();
+    
+    const quoteRequest = new Promise((resolve, reject) => {
+        xhr.open('GET', 'https://type.fit/api/quotes');
+        xhr.send();
+        
+        xhr.onload = event => {
+            return resolve(xhr.response);
+        }
+
+        xhr.onerror = errEvent => {
+            return reject('Something went wrong, Check your Api or internet connection and then try again !');
+        }
+    });
+
+    quoteRequest.then(res => {
+        const quotes = JSON.parse(res);
+        const quotesLength = quotes.length;
+        const randomQuoteIndex = Math.floor(Math.random() * quotesLength);
+        const selectedRandomQuote = quotes[randomQuoteIndex];
+        quoteDes.textContent = selectedRandomQuote.text;
+        quoteAuthor.textContent = selectedRandomQuote.author;
+    })
+    .catch(err => {
+        console.log(err);
+    })
+}
+
+nextQuoteBtn.addEventListener('click', randomQuoteHandler);
